b3fe83c21b9d6cb967a96af1c7a71845478837df
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2023-12-12 19:43:40 +0630

    no_cartesian

--- src/gps_frame.cpp
+++ src/gps_frame.cpp
@@ -5,6 +5,13 @@
 
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
 #include <visualization_msgs/msg/marker.hpp>
+#include <GeographicLib/LocalCartesian.hpp>
+#include <GeographicLib/Geocentric.hpp>
+#include <geometry_msgs/msg/point.hpp>
+
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
 
 
 
@@ -12,19 +19,16 @@ class Gps_transformer_node : public rclcpp::Node {
 public:
     Gps_transformer_node(): Node("Gps_transformer_node") {
 
-        double lat_origin, lon_origin, alt_origin;
-        bool first_frame = true;    
-              
-
-        GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(),
-                                GeographicLib::Constants::WGS84_f());
-        GeographicLib::LocalCartesian proj_geo;
+ 
+        // LocalCartesian proj_geo;
 
         gps_subscriber_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(
-            "/fix", 10, std::bind(&Gps_transformer_node::gps_callback, this, std::placeholders::_1));
+            "/gps/micro", 10, std::bind(&Gps_transformer_node::gps_callback, this, std::placeholders::_1));
 
 
-        visualization_msgs::msg::Marker line_strip;
+        marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("/gps_marker", 10);
+
+        tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(this);
 
 
 
@@ -32,12 +36,98 @@ public:
 
 private:
 
-     void gps_callback( const std::shared_ptr<sensor_msgs::msg::NavSatFix> msg) {
-       
+
+    void gps_callback( const std::shared_ptr<sensor_msgs::msg::NavSatFix> msg) {
+
+        double lat_origin, lon_origin, alt_origin;
+        bool first_frame = true;
+
+        visualization_msgs::msg::Marker line_strip;    
+
+        
+        GeographicLib::LocalCartesian local_geo;
+        GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(),
+                                GeographicLib::Constants::WGS84_f());
+
+        if (first_frame) {
+            lat_origin = msg->latitude;
+            lon_origin = msg->longitude;
+            alt_origin = 0.0;
+            local_geo = GeographicLib::LocalCartesian(lat_origin,
+                                                    lon_origin,
+                                                    alt_origin,
+                                                    earth);
+        first_frame = false;
+            
+        }
+
+
+          double x, y, z;
+  // Warning: if the measurement of altitude is wrong,
+  // then the result of projection will be wrong.
+        local_geo.Forward(msg->latitude, msg->longitude, 0.0, x, y, z);
+
+
+        std::cout << "Status: " << static_cast<int>(msg->status.status) << std::endl;
+        std::cout << "(latitude, longitude, altitude)(deg, m) => (x, y, z)(m) = ("
+            << std::to_string(msg->latitude) << ", "
+            << std::to_string(msg->longitude) << ", "
+            << std::to_string(0.0) << ") => ("
+            << x << ", "
+            << y << ", "
+            << z << ")" << std::endl;
+    
+        // RCLCPP_INFO(this->get_logger(), x);
+
+
+        geometry_msgs::msg::Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+
+        line_strip.points.push_back(p);
+        if (line_strip.points.size() > 10000)
+        line_strip.points.clear();
+
+
+        
+        line_strip.header.frame_id = "/linestrip";
+        line_strip.ns = "linestrip";
+        line_strip.action = visualization_msgs::msg::Marker::ADD;
+        line_strip.pose.orientation.w = 1.0;
+        line_strip.id = 1;
+        line_strip.type = visualization_msgs::msg::Marker::LINE_STRIP;
+
+        line_strip.scale.x = 0.5;
+        line_strip.color.r = 1.0;
+        line_strip.color.a = 1.0;
+
+        marker_publisher_->publish(line_strip);
+
+
+        geometry_msgs::msg::TransformStamped transform_stamped;
+
+        transform_stamped.header.stamp = this->get_clock()->now();
+        transform_stamped.header.frame_id ="map";  // Parent frame
+        transform_stamped.child_frame_id = "/linestrip";   // Child frame
+        transform_stamped.transform.translation.x = 0;  
+        transform_stamped.transform.translation.y = 0.0;
+        transform_stamped.transform.translation.z = 0.27; 
+
+        tf2::Quaternion q;
+        q.setRPY(0, 0, 0);
+        transform_stamped.transform.rotation.x = q.x();
+        transform_stamped.transform.rotation.y = q.y();
+        transform_stamped.transform.rotation.z = q.z();
+        transform_stamped.transform.rotation.w = q.w();
+
+        tf_broadcaster_->sendTransform(transform_stamped);
 
      }
 
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher_;
     rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_subscriber_;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
 };
 
